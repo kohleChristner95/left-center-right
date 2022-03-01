82d68c63f44af30509ab24e4cf608acfc9d6892a
Kohle Christner <80296696+kohleChristner95@users.noreply.github.com>
2022-02-28 21:02:24 -0500

    Started adding more styling to player box

--- src/Components/Player.tsx
+++ src/Components/Player.tsx
@@ -1,11 +1,16 @@
 import player from "./img/player.png";
+import token from "./img/token.jpg";
 
 const Player = () => {
   return (
     <div>
       <div>Player 1</div>
       <img className="player-img" src={player} alt=""></img>
-      <div className="token-block">hello</div>
+      <div className="token-block">
+        <img className="token-piece" src={token} alt=""></img>
+        <img className="token-piece" src={token} alt=""></img>
+        <img className="token-piece" src={token} alt=""></img>
+      </div>
     </div>
   );
 };
